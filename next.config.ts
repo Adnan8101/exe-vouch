d0b233dc3d0aba8cf6a2a75dc619677e312bb68a
Adnan8101 <adnanqureshi8101@gmail.com>
2025-11-17 16:44:49 +0530

    fixed config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
 import type { NextConfig } from "next";
 
-import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
@@ -32,9 +30,6 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
   compress: true,
-  swcMinify: true,
 };
-
-export default nextConfig;
 
 export default nextConfig;
